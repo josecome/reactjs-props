ba703a2a5a38e5766127fa23da78e9a703bc3737
Jose Jaime Come <josejaimecome@gmail.com>
2023-03-02 19:58:26 +0200

    Minor changes

--- src/App.js
+++ src/App.js
@@ -11,9 +11,31 @@ function App() {
            brand="Toyota"
            serie="Corrola"
            origin="Japan"
-           price={60000.00}          
+           price={60000.00} 
         />
-     
+
+        <Car 
+           car_id={2}
+           brand="BMW"
+           serie="320i"
+           origin="German"
+           price={80000.00}          
+        />     
+        <Car 
+           car_id={3}
+           brand="Mercedes-Benz"
+           serie="c180"
+           origin="German"
+           price={90000.00}          
+        />
+
+        <Car 
+           car_id={4}
+           brand="Volvo"
+           serie="s60"
+           origin="Sweden"
+           price={78000.00}          
+        />  
     </div>
   );
 }
