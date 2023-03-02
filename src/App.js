0c34b9f62e4b84166e649ea25951480553652af6
Jose Jaime Come <josejaimecome@gmail.com>
2023-03-02 19:34:15 +0200

    Minor changes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Props in React</h1>
+
+        <Car 
+           car_id={1}
+           brand="Toyota"
+           serie="Corrola"
+           origin="Japan"
+           price={60000.00}          
+        />
+     
     </div>
   );
 }
